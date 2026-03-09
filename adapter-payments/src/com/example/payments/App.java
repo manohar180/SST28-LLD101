5d21098f07685f1594e7a5dfe3ffbc6483130654
manohar180 <vanukurimanoharreddy77@gmail.com>
2026-03-09 11:19:09 +0530

    Implemented proxy pattern

--- adapter-payments/src/com/example/payments/App.java
+++ adapter-payments/src/com/example/payments/App.java
@@ -6,14 +6,16 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) {
         Map<String, PaymentGateway> gateways = new HashMap<>();
-        // TODO: register adapters instead of raw SDKs
-        // gateways.put("fastpay", new FastPayAdapter(new FastPayClient()));
-        // gateways.put("safecash", new SafeCashAdapter(new SafeCashClient()));
+        gateways.put("fastpay", new FastPayAdapter(new FastPayClient()));
+        gateways.put("safecash", new SafeCashAdapter(new SafeCashClient()));
+
         OrderService svc = new OrderService(gateways);
 
+        System.out.println("=== Processing Payments ===");
         String id1 = svc.charge("fastpay", "cust-1", 1299);
         String id2 = svc.charge("safecash", "cust-2", 1299);
-        System.out.println(id1);
-        System.out.println(id2);
+        
+        System.out.println("FastPay Result: " + id1);
+        System.out.println("SafeCash Result: " + id2);
     }
 }
